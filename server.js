b652fb2b8d710484376caae0da5dcdf82654c730
Lokendra-coder <arunkushwah949@gmail.com>
2026-05-17 15:48:53 +0530

    fix json body limit

--- server.js
+++ server.js
@@ -12,14 +12,17 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://ai-attendance-frontend-rho.vercel.app"],
+    origin: [
+      "http://localhost:5173",
+      "https://ai-attendance-frontend-rho.vercel.app",
+    ],
     credentials: true,
   })
 );
-app.use(express.json());
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use("/api/students", studentRoutes);
